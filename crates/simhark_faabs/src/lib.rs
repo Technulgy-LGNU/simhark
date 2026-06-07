e02ccf7a15e5d8eefd399c0d78cab204414537a7
Shark <sharktheone@proton.me>
2026-06-07 15:31:21 +0200

    add interface and main

--- crates/simhark_faabs/src/lib.rs
+++ crates/simhark_faabs/src/lib.rs
@@ -1,71 +1,170 @@
 mod conv;
+#[cfg(feature = "interface")]
+mod interface;
 mod run;
 
-use std::mem;
-use tf_jetsoncode::Robot;
-use ::CrashPilot::{CrashPilot, core_dump};
-use simhark::{World, WorldCommand, WorldState};
 use crate::conv::world_state_to_cp_events;
+#[cfg(feature = "interface")]
+use crate::interface::EventShare;
 use crate::run::run_sim_action;
+use simhark::{WorldCommand, WorldState};
+use std::collections::HashMap;
+use std::mem;
+use std::net::Ipv4Addr;
+use tf_jetsoncode::Robot;
+use ::CrashPilot::config::{LoggingConfig, RobotConfig, ServerConfig, SslConfig};
+use ::CrashPilot::CrashPilot;
 
 pub struct Faabs {
     pub robots: Vec<Robot<()>>,
     pub crash_pilot: CrashPilot<()>,
     pub feedback_robot: u32,
     pub events: ::CrashPilot::Events,
-
+    #[cfg(feature = "interface")]
+    pub interface: EventShare,
+    #[cfg(feature = "interface")]
+    pub ws_out: ::CrashPilot::communication::WebsocketOut,
 }
 
 impl Faabs {
+    #[cfg(feature = "interface")]
+    pub fn with_interface(num_robots: u8) -> Self {
+        let faabs = Self::new(num_robots);
+
+        #[cfg(feature = "interface")]
+        {
+            let cfg = get_config();
+            let tx = faabs.interface.clone();
+            let ws_out = faabs.ws_out.clone();
+
+            ::CrashPilot::interface::spawn_interface();
+
+            tokio::spawn(async move {
+                crate::interface::spawn_websocket(&cfg, tx, ws_out).await;
+            });
+        }
+
+        faabs
+    }
+
     pub fn new(num_robots: u8) -> Self {
         let mut robots = Vec::with_capacity(num_robots as usize);
 
         for i in 0..num_robots {
             let mut config = tf_jetsoncode::Config::default();
-            config.robot_id = i + 1;
-
+            config.robot_id = i;
 
             robots.push(Robot::new(config));
-
         }
-
 
         Self {
             robots,
-            crash_pilot: CrashPilot::new(::CrashPilot::Config::default()),
+            crash_pilot: CrashPilot::new(get_config()),
             feedback_robot: 0,
             events: ::CrashPilot::Events::default(),
+            #[cfg(feature = "interface")]
+            interface: EventShare::default(),
+            #[cfg(feature = "interface")]
+            ws_out: ::CrashPilot::communication::WebsocketOut::new(),
         }
     }
 
     pub fn step(&mut self, state: &WorldState, command: &mut WorldCommand) {
         world_state_to_cp_events(&mut self.events, state);
 
-        let (_interface, robots)= self.crash_pilot.step_with_data(mem::take(&mut self.events));
+        #[cfg(feature = "interface")]
+        {
+            self.events.ws = self.interface.blocking_lock().clone();
+        }
 
+        let ws = self.events.ws.clone();
 
+        let (interface, robots) = self.crash_pilot.step_with_data(mem::take(&mut self.events));
+
+        #[cfg(feature = "interface")]
+        {
+            self.ws_out.publish_sync(interface);
+        }
+
+        self.events.ws = ws;
 
         for (id, data) in robots {
-            let Some(robot) = self.robots.get_mut((id - 1) as usize) else {
-                continue;
+            let Some(robot) = self.robots.get_mut(id as usize) else {
+                panic!(
+                    "Received data for robot with id {}, but only {} robots are configured",
+                    id,
+                    self.robots.len()
+                );
             };
+
 
             let events = conv::robot_events(id, data, state);
 
             let (teensy, robot_cp) = robot.step_with_data(events);
+
 
             run_sim_action(id, teensy, command);
 
             if self.feedback_robot == id {
                 self.events.rf = Some(conv::robot_cp(robot_cp));
             }
-
         }
 
         self.feedback_robot += 1;
         self.feedback_robot %= self.robots.len() as u32;
+    }
+}
 
+fn get_config() -> ::CrashPilot::Config {
+    let mut robots = HashMap::new();
 
+    robots.insert(
+        0,
+        RobotConfig {
+            ip: Ipv4Addr::new(10, 0, 64, 101),
+            substitution_pos: Default::default(),
+        },
+    );
+    robots.insert(
+        1,
+        RobotConfig {
+            ip: Ipv4Addr::new(10, 0, 64, 101),
+            substitution_pos: Default::default(),
+        },
+    );
+    robots.insert(
+        2,
+        RobotConfig {
+            ip: Ipv4Addr::new(10, 0, 64, 102),
+            substitution_pos: Default::default(),
+        },
+    );
+    robots.insert(
+        3,
+        RobotConfig {
+            ip: Ipv4Addr::new(10, 0, 64, 103),
+            substitution_pos: Default::default(),
+        },
+    );
+    robots.insert(
+        4,
+        RobotConfig {
+            ip: Ipv4Addr::new(10, 0, 64, 104),
+            substitution_pos: Default::default(),
+        },
+    );
+    robots.insert(
+        5,
+        RobotConfig {
+            ip: Ipv4Addr::new(10, 0, 64, 104),
+            substitution_pos: Default::default(),
+        },
+    );
 
+    ::CrashPilot::Config {
+        ssl: SslConfig::default(),
+        server: ServerConfig::default(),
+        logging: LoggingConfig::default(),
+        robots,
     }
 }
---
+++ crates/simhark_faabs/src/main.rs
@@ -0,0 +1,37 @@
+use std::thread;
+use simhark::{SimulationEngine, WorldCommand, WorldConfig};
+use simhark_faabs::Faabs;
+
+fn main() {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            tokio::task::spawn_blocking(run).await.unwrap();
+        });
+}
+
+fn run() {
+    let config = WorldConfig::division_b();
+
+    let mut engine = SimulationEngine::new(1, config.clone());
+
+    let mut faabs = Faabs::with_interface(6);
+
+    let command = WorldCommand::default();
+
+    let mut state = engine.step_with_commands(&[command]).remove(0);
+
+    loop {
+        let mut command = WorldCommand::default();
+
+        faabs.step(&state, &mut command);
+
+        // dbg!(&command);
+
+        state = engine.step_with_commands(&[command]).remove(0);
+
+        thread::sleep(std::time::Duration::from_millis(2));
+    }
+}
